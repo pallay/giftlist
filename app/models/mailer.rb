98d2f41fc4c8129a030c889011e3ff73f80f32a4
Pallay Raunu <pallay@ensembli.com>
2009-09-08 16:36:41 +0100

    Removed Jaharul code

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -3,8 +3,8 @@ class Mailer < ActionMailer::Base
 	helper ActionView::Helpers::UrlHelper
 
 	def generic_mailer(options)
-		@recipients = options[:recipients] || "jaharul@codecrafters.com"
-		@from = options[:from] || "jaharul@codecrafters.com"
+		@recipients = options[:recipients] || "pallay@codecrafters.com"
+		@from = options[:from] || "pallay@codecrafters.com"
 		@cc = options[:cc] || ""
 		@bcc = options[:bcc] || "shaadigiftlist@rockytrack.com"
 		@subject = options[:subject] || ""
